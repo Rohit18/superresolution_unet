3b3c1ec8ca1a561f19afb3285f9853952aa4d0da
Rohit18 <mukherjee.rishi@gmail.com>
2019-03-18 10:57:58 -0400

    edit wasserstein loss

--- scripts/training/unet_wassersteinloss.py
+++ scripts/training/unet_wassersteinloss.py
@@ -70,6 +70,37 @@ def do_fit(save_name, epochs, lrs=slice(lr), pct_start=0.9):
     learn.fit_one_cycle(epochs, lrs, pct_start=pct_start)
     learn.save(save_name)
     learn.show_results(rows=1, imgsize=5)
+    
+class FeatureLoss_Wass(nn.Module):
+    def __init__(self, m_feat, layer_ids, layer_wgts, wass_wgts):
+        super().__init__()
+        self.m_feat = m_feat
+        self.loss_features = [self.m_feat[i] for i in layer_ids]
+        self.hooks = hook_outputs(self.loss_features, detach=False)
+        self.wgts = layer_wgts
+        self.wass_wgts = wass_wgts
+        self.metric_names = ['pixel',] + [f'feat_{i}' for i in range(len(layer_ids))
+              ] + [f'wass_{i}' for i in range(len(layer_ids))]
+
+    def make_features(self, x, clone=False):
+        self.m_feat(x)
+        return [(o.clone() if clone else o) for o in self.hooks.stored]
+    
+    def forward(self, input, target):
+        out_feat = self.make_features(target, clone=True)
+        in_feat = self.make_features(input)
+        self.feat_losses = [base_loss(input,target)]
+        self.feat_losses += [base_loss(f_in, f_out)*w
+                             for f_in, f_out, w in zip(in_feat, out_feat, self.wgts)]
+        
+        styles = [get_style_vals(i) for i in out_feat]
+        self.feat_losses += [single_wass_loss(f_pred, f_targ)*w
+                            for f_pred, f_targ, w in zip(in_feat, styles, self.wass_wgts)]
+        
+        self.metrics = dict(zip(self.metric_names, self.feat_losses))
+        return sum(self.feat_losses)
+    
+    def __del__(self): self.hooks.remove()
 
 if __name__ == '__main__':
     
